b8c5c54db723ef685c600b1d707895b2670d89c5
Muhammed Ashfaq <ahfaqmohammedcv@gmail.com>
2021-10-07 21:41:22 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function validate() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    if (username == 'user' && password == '12345') {
+        alert('Login Succesfully');
+        return false
+    }
+    else {
+        alert('Login Failed');
+    }
+
+
+}
